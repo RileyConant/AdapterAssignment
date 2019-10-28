9fbc0d70d35b5a58fedc4ac45bbccfd70f0dd766
RileyConant <rconant@email.sc.edu>
2019-10-27 21:26:02 -0400

    fixing my idention mistakes

--- AdapterAssignment/src/WizardAdapter.java
+++ AdapterAssignment/src/WizardAdapter.java
@@ -4,26 +4,26 @@
  *
  */
 public class WizardAdapter implements Warrior{
-private Wizard wizard;
-/**
- * Constructor of WizardAdapter
- * @param w Wizard object that will be converted
- */
-public WizardAdapter(Wizard w) {
-	this.wizard = w;
-}
-/**
- * Calls the performSpell from the wizard object
- */
-@Override
-public void attack() {
-	wizard.performSpell();
-}
-/**
- * Calls the wizards heal function
- */
-@Override
-public void defend() {
-	wizard.heal();
-}
+	private Wizard wizard;
+	/**
+	 * Constructor of WizardAdapter
+	 * @param w Wizard object that will be converted
+	 */
+	public WizardAdapter(Wizard w) {
+		this.wizard = w;
+	}
+	/**
+	 * Calls the performSpell from the wizard object
+	 */
+	@Override
+	public void attack() {
+		wizard.performSpell();
+	}
+	/**
+	 * Calls the wizards heal function
+	 */
+	@Override
+	public void defend() {
+		wizard.heal();
+	}
 }
